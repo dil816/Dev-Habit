85582c3006c3975c72b3ecd6a92ca831de9c4203
dil816 <Rivindudilochana18329@gmail.com>
2025-04-17 13:50:28 +0530

    content negotiation

--- DevHabit/DevHabitApi/Controllers/HabitsController.cs
+++ DevHabit/DevHabitApi/Controllers/HabitsController.cs
@@ -59,20 +59,25 @@ public sealed class HabitsController(ApplicationDbContext dbContext, LinkService
             .Take(query.PageSize)
             .ToListAsync();
 
+        bool includeLinks = query.Accept == CustomMediaTypeNames.Application.HateoasJson;
+
         var paginationResult = new PaginationResult<ExpandoObject>
         {
             Items = dataShapingService.ShapeCollectionData(
                 habits,
                 query.Fields,
-                h => CreateLinksForHabit(h.Id, query.Fields)),
+                includeLinks ? h => CreateLinksForHabit(h.Id, query.Fields) : null),
             Page = query.Page,
             PageSize = query.PageSize,
             TotalCount = totalCount
         };
-        paginationResult.Links = CreateLinksForHabit(
+        if (includeLinks)
+        {
+            paginationResult.Links = CreateLinksForHabit(
             query,
             paginationResult.HasNextPage,
             paginationResult.HasPreviousPage);
+        }
 
         return Ok(paginationResult);
     }
@@ -81,6 +86,8 @@ public sealed class HabitsController(ApplicationDbContext dbContext, LinkService
     public async Task<IActionResult> GetHabit(
         string id,
         string? fields,
+        [FromHeader(Name = "Accept")]
+        string? accept,
         DataShapingService dataShapingService)
     {
         if (!dataShapingService.Validate<HabitWithTagsDto>(fields))
@@ -103,9 +110,12 @@ public sealed class HabitsController(ApplicationDbContext dbContext, LinkService
 
         ExpandoObject shapedHabitDto = dataShapingService.ShapeData(habit, fields);
 
-        List<LinkDto> links = CreateLinksForHabit(id, fields);
+        if (accept == CustomMediaTypeNames.Application.HateoasJson)
+        {
+            List<LinkDto> links = CreateLinksForHabit(id, fields);
 
-        shapedHabitDto.TryAdd("links", links);
+            shapedHabitDto.TryAdd("links", links);
+        }
 
         return Ok(shapedHabitDto);
     }
--- DevHabit/DevHabitApi/DependencyInjection.cs
+++ DevHabit/DevHabitApi/DependencyInjection.cs
@@ -5,6 +5,8 @@ using DevHabitApi.Middleware;
 using DevHabitApi.Services;
 using DevHabitApi.Services.Sorting;
 using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Newtonsoft.Json.Serialization;
@@ -27,6 +29,15 @@ public static class DependencyInjection
         .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver =
             new CamelCasePropertyNamesContractResolver())
         .AddXmlSerializerFormatters();
+
+        //adding custom media type "Accept"
+        builder.Services.Configure<MvcOptions>(options =>
+        {
+            NewtonsoftJsonOutputFormatter formatter = options.OutputFormatters
+                .OfType<NewtonsoftJsonOutputFormatter>()
+                .First();
+            formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateoasJson);
+        });
 
         builder.Services.AddOpenApi();
 
