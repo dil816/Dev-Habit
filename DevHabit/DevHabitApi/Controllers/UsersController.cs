b8cf98446ed878fbbce47a52f63db467dfa7cd1e
dil816 <Rivindudilochana18329@gmail.com>
2025-04-18 18:03:16 +0530

    added user resources

---
+++ DevHabit/DevHabitApi/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using DevHabitApi.Database;
+using DevHabitApi.DTOs.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabitApi.Controllers;
+
+[ApiController]
+[Route("users")]
+internal sealed class UsersController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetUserById(string id)
+    {
+        UserDto? user = await dbContext.Users
+            .Where(u => u.Id == id)
+            .Select(UserQueries.ProjectToDto())
+            .FirstOrDefaultAsync();
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+}
---
+++ DevHabit/DevHabitApi/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace DevHabitApi.Entities;
+
+public sealed class User
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime? UpdatedAtUtc { get; set; }
+
+    /// <summary>
+    /// We'll use this to store the identityId from the Identity Provider.
+    /// This could be any identity provider like Azure AD, Cognito, Keyloak, Okta, Auth0, etc
+    /// </summary>
+    public string IdentityId { get; set; }
+}
